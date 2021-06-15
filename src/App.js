7b3dbabdd5efa0191f2a89ccbeb10ac5cec84669
Duy Nhat <duynhat719@gmail.com>
2021-06-15 21:10:11 +0700

    clear features folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.scss";
-import AlbumFeature from "./features/Album";
 
 function App() {
   return (
     <div className="App">
-      <h1>React Music Album from mp3</h1>
-      <AlbumFeature />
+      <h1>React Music Album From Mp3</h1>
     </div>
   );
 }
