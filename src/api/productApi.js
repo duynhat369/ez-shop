4ed59fe361931e4a61d5de56178e640b37a26c8c
Duy Nhat <duynhat719@gmail.com>
2021-08-15 09:40:51 +0700

    API getAll product list

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -1,9 +1,33 @@
 import axiosClient from './axiosClient';
 
 const productApi = {
-  getAll(params) {
+  async getAll(params) {
     const url = '/products';
-    return axiosClient.get(url, { params });
+    // return axiosClient.get(url, { params });
+
+    // + async getAll
+    // + transform _page to _start
+    const newParams = { ...params };
+    console.log({ params });
+    newParams._start =
+      !params._page || params._page <= 1 ? 0 : (params._page - 1) * (params._limit || 50);
+
+    // + remove un-needed key
+    delete newParams._page;
+
+    // + fetch product list + count
+    const productList = await axiosClient.get(url, { params: newParams });
+    const count = await axiosClient.get(`${url}/count`, { params: newParams });
+
+    // + build response and return
+    return {
+      data: productList,
+      pagination: {
+        page: params._page,
+        limit: params._limit,
+        total: count,
+      },
+    };
   },
 
   get(id) {
